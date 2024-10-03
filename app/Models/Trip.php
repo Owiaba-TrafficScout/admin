d1b59778a640d63802eb777d02bb1b671061161a
anamboijohn <anamboijohn@gmail.com>
2024-10-03 21:46:04 +0000

    define relationship between trip and TripStops

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Trip extends Model
 {
@@ -21,5 +22,10 @@ class Trip extends Model
     public function speed(): BelongsTo
     {
         return $this->belongsTo(TripSpeed::class);
+    }
+
+    public function stops(): HasMany
+    {
+        return $this->hasMany(TripStop::class);
     }
 }
