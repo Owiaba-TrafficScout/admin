a63bddf4eef73f802efe68a2907046370c28d877
anamboijohn <anamboijohn@gmail.com>
2024-10-03 20:55:15 +0000

    define relationship between car and car status

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Car extends Model
 {
     /** @use HasFactory<\Database\Factories\CarFactory> */
     use HasFactory;
     protected $guarded = [];
+
+    public function status(): BelongsTo
+    {
+        return $this->belongsTo(CarStatus::class, 'car_status_id');
+    }
 }
--- app/Models/CarStatus.php
+++ app/Models/CarStatus.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class CarStatus extends Model
 {
@@ -11,4 +12,9 @@ class CarStatus extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function cars(): HasMany
+    {
+        return $this->hasMany(Car::class);
+    }
 }
