7e32f57dfcfb4191d221db174146e9639553ece4
anamboijohn <anamboijohn@gmail.com>
2024-10-03 21:38:34 +0000

    create TripStop resources

---
+++ app/Http/Controllers/TripStopController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TripStopController extends Controller
+{
+    //
+}
---
+++ app/Models/TripStop.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TripStop extends Model
+{
+    /** @use HasFactory<\Database\Factories\TripStopFactory> */
+    use HasFactory;
+}
