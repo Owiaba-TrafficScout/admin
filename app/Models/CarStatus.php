11567774c8561092a7bec455af72a2d7503bdf47
anamboijohn <anamboijohn@gmail.com>
2024-10-03 20:44:26 +0000

    create CarStatus resources

---
+++ app/Models/CarStatus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CarStatus extends Model
+{
+    /** @use HasFactory<\Database\Factories\CarStatusFactory> */
+    use HasFactory;
+}
