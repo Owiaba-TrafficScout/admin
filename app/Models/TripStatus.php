f57760e29f235d82338dac8f790656fbe2a97987
anamboijohn <anamboijohn@gmail.com>
2024-10-03 21:27:09 +0000

    create trip status resources

---
+++ app/Models/TripStatus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TripStatus extends Model
+{
+    /** @use HasFactory<\Database\Factories\TripStatusFactory> */
+    use HasFactory;
+}
