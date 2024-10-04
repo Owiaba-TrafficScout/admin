9d3e21fc7b130e2f383d4d71a2ddfb3a06575b32
anamboijohn <anamboijohn@gmail.com>
2024-10-04 07:04:40 +0000

    define route and controller function

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Trip;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class TripController extends Controller
 {
-    //
+    public function index()
+    {
+        $trips = Trip::all();
+        return Inertia::render('Trips', ['trips' => $trips]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TripController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -21,10 +22,14 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard2');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Trips routes
+    Route::get('/trips', [TripController::class, 'index'])->name('trips.index');
 });
 
 require __DIR__ . '/auth.php';
