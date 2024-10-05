423a9a21b2f620c50b31cf38f1bc5fe55ef0bd46
anamboijohn <anamboijohn@gmail.com>
2024-10-05 16:10:27 +0000

    refactor code

--- resources/js/Layouts/App.vue
+++ resources/js/Layouts/App.vue
@@ -8,7 +8,6 @@ import {
     Menu,
     Package,
     Package2,
-    Search,
     ShoppingCart,
     Users,
 } from 'lucide-vue-next';
@@ -23,7 +22,6 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from '@/Components/ui/dropdown-menu';
-import { Input } from '@/Components/ui/input';
 import { Sheet, SheetContent, SheetTrigger } from '@/Components/ui/sheet';
 import { Link } from '@inertiajs/vue3';
 
@@ -178,18 +176,7 @@ defineProps<{
                     </SheetContent>
                 </Sheet>
                 <div class="w-full flex-1">
-                    <form>
-                        <div class="relative">
-                            <Search
-                                class="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground"
-                            />
-                            <Input
-                                type="search"
-                                placeholder="Search products..."
-                                class="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-                            />
-                        </div>
-                    </form>
+                    {{ 'Welcome ' + $page.props.auth.user.name + '!' }}
                 </div>
                 <DropdownMenu>
                     <DropdownMenuTrigger as-child>
