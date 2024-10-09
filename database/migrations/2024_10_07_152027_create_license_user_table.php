e387ed36878937b8944e0faa9ac1183cb88e1af4
anamboijohn <anamboijohn@gmail.com>
2024-10-09 08:51:22 +0000

    fix bug: add user_id to many to many relationship table licence_user

--- database/migrations/2024_10_07_152027_create_license_user_table.php
+++ database/migrations/2024_10_07_152027_create_license_user_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('license_user', function (Blueprint $table) {
             $table->id();
             $table->foreignId('license_id')->constrained()->cascadeOnDelete();
-            
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
