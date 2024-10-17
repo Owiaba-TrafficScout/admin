2919befd238bab09c41e29a3b750434ded66cc5c
anamboijohn <anamboijohn@gmail.com>
2024-10-17 14:56:42 +0000

    create TenantUser resources

---
+++ app/Models/TenantUser.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class TenantUser extends Pivot
+{
+    /** @use HasFactory<\Database\Factories\TenantUserFactory> */
+    use HasFactory;
+}
