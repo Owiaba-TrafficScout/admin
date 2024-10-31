6a198d6a7a2bb046df865ee5a414e3c8e4110d5f
anamboijohn <anamboijohn@gmail.com>
2024-10-31 11:44:38 +0000

    set default time stamps

--- database/migrations/2024_10_16_141440_create_subscriptions_table.php
+++ database/migrations/2024_10_16_141440_create_subscriptions_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -16,8 +17,8 @@ return new class extends Migration
             $table->foreignId('plan_id')->constrained('subscription_plans')->cascadeOnDelete();
             $table->foreignId('tenant_id')->constrained()->cascadeOnDelete();
             $table->timestamp('trial_ends_at')->nullable();
-            $table->timestamp('start_date');
-            $table->timestamp('end_date');
+            $table->timestamp('start_date')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('end_date')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->foreignId('status_id')->constrained('subscription_statuses')->cascadeOnDelete();
             $table->timestamps();
         });
