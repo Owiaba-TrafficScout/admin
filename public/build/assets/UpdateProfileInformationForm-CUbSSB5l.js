005fd297d8d8cf178f7f48eb09313f45aea4ccfa
anamboijohn <anamboijohn@gmail.com>
2024-11-05 15:15:54 +0000

    add build folder

---
+++ public/build/assets/UpdateProfileInformationForm-CUbSSB5l.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./UpdateProfileInformationForm.vue_vue_type_script_setup_true_lang-BTIG_uXI.js";import"./app-C5IldEcd.js";import"./TextInput.vue_vue_type_script_setup_true_lang-COyazjSy.js";import"./InputLabel.vue_vue_type_script_setup_true_lang-BuhHF0Xy.js";import"./PrimaryButton-BLr2sEaB.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";export{o as default};
