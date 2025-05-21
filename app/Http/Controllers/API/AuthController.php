d66e1bb2dd2a894f59293973988116605e53ad7c
anamboijohn <anamboijohn@gmail.com>
2025-05-21 14:09:44 +0000

    add auth apis

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => ['required', 'max:55'],
+            'email' => ['email', 'required', 'unique:users'],
+            'password' => ['confirmed', Rules\Password::defaults()],
+        ]);
+
+        $validatedData['password'] = bcrypt($request->password);
+
+
+        $user = User::create($validatedData);
+
+        $token = $user->createToken('authToken');
+
+        return response(
+            [
+                'user' => $user,
+                'access_token' => $token->plainTextToken,
+                'message' => 'Account created successfully',
+            ],
+            200
+        );
+    }
+
+    // Login
+    public function login(Request $request)
+    {
+        $validatedData = $request->validate([
+            'email' => ['email', 'required'],
+            'password' => ['required', 'min:8'],
+        ]);
+
+        if (!Auth::attempt($validatedData)) {
+            return response(['message' => 'Invalid credentials'], 401);
+        }
+        $user = Auth::user();
+        return response([
+            'user' => $user,
+            'token' => $user->createToken('authToken')->plainTextToken,
+            'message' => 'Logged in successfully!',
+        ], 200);
+    }
+
+    // User
+    public function user(Request $request)
+    {
+        return response(['user' => $request->user()], 200);
+    }
+    // Logout
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response(['message' => 'Logged out successfully!'], 200);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\API\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [AuthController::class, 'register'])->name('api.register');
+Route::post('/login', [AuthController::class, 'login']);
+Route::group(['prefix' => 'auth:sanctum'], function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
