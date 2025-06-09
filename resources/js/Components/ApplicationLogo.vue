4cc1b0163c27c79efb4187eeeb319578086a2829
anamboijohn <anamboijohn@gmail.com>
2025-06-09 23:58:20 +0000

    Remove logo.png and update ApplicationLogo.vue to use logo from assets

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,3 +1,7 @@
 <template>
-    <img src="/logo.png" alt="Application Logo" class="h-12 w-auto" />
+    <img :src="logo" alt="Application Logo" class="h-12 w-auto" />
 </template>
+
+<script setup>
+import logo from '@/assets/logo.png';
+</script>
