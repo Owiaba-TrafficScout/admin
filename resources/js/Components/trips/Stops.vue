5dcaeedcbf4da541f10a10c19a9b3cee423cb898
anamboijohn <anamboijohn@gmail.com>
2025-08-04 23:33:19 +0000

    Simplify traffic status display in Speeds and Stops components

--- resources/js/Components/trips/Stops.vue
+++ resources/js/Components/trips/Stops.vue
@@ -147,7 +147,7 @@ defineProps<{
                         <p
                             class="text-sm font-semibold text-black dark:text-white"
                         >
-                            {{ stop.is_traffic ? 'Yes' : 'No' }}
+                            {{ stop.is_traffic }}
                         </p>
                     </div>
                 </div>
